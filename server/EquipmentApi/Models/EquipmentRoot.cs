0f7b5ef8717ab58aa1a63ceea690bbc36b12ad24
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-08-22 18:30:12 +0100

    removes unnecessary constructor in EQuipmentRoot

--- server/EquipmentApi/Models/EquipmentRoot.cs
+++ server/EquipmentApi/Models/EquipmentRoot.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace EquipmentApi.Models
 {
     public class EquipmentRoot
     {
-        public EquipmentRoot()
-        {
-        }
+        private List<SerialisedEquipment> _serialisedEquipment;
+        private List<EquipmentType> _equipmentType;
+
+        public List<SerialisedEquipment> SerialisedEquipment { get => _serialisedEquipment; set => _serialisedEquipment = value; }
+        public List<EquipmentType> EquipmentType { get => _equipmentType; set => _equipmentType = value; }
     }
 }
