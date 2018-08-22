0526875b4c0cc514ca15036ee0b1705b60d392b6
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-08-22 18:27:28 +0100

    creates SerialisedEquipment class

---
+++ server/EquipmentApi/Models/SerialisedEquipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EquipmentApi.Models
+{
+    public class SerialisedEquipment
+    {
+        private string _id;
+        private string _externalId;
+        private string EquipmentTypeId { get; set; }
+
+        string Id { get => _id; set => _id = value; }
+        string ExternalId { get => _externalId; set => _externalId = value; }
+
+        int MeterReading { get => _meterReading; set => _meterReading = value; }
+    }
+}
