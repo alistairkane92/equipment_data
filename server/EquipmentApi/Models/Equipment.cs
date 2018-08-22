6fc01ea684e81b34f93760255079381c3ad246c6
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-08-22 21:14:13 +0100

    adds equipment constructor and refactors equipmentRoot to be non static

--- server/EquipmentApi/Models/Equipment.cs
+++ server/EquipmentApi/Models/Equipment.cs
@@ -1,6 +1,35 @@
 ﻿namespace EquipmentApi.Models
 {
-    internal class Equipment
+    public class Equipment
     {
+        private string _id;
+        private string _externalId;
+        private string _equipmentTypeId;
+        private int _meterReading;
+
+        private string _typeId;
+        private string _typeExternalId;
+        private string _typeDescription;
+
+        public string Id { get => _id; set => _id = value; }
+        public string ExternalId { get => _externalId; set => _externalId = value; }
+        public string EquipmentTypeId { get => _equipmentTypeId; set => _equipmentTypeId = value; }
+        public int MeterReading { get => _meterReading; set => _meterReading = value; }
+
+        public string TypeId { get => _typeId; set => _typeId = value; }
+        public string TypeExternalId { get => _typeExternalId; set => _typeExternalId = value; }
+        public string TypeDescription { get => _typeDescription; set => _typeDescription = value; }
+
+        public Equipment(string id, string externalId, string equipmentTypeId, int meterReading, string typeId, string typeExternalId, string typeDescription)
+        {
+            this.Id = id;
+            this.ExternalId = externalId;
+            this.EquipmentTypeId = equipmentTypeId;
+            this.MeterReading = meterReading;
+            this.TypeId = typeId;
+            this.TypeExternalId = typeExternalId;
+            this.TypeDescription = typeDescription;
+        }
+
     }
 }
