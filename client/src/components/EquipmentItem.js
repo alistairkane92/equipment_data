2077594d2bac3e81d7e646e77870dd262cb3e163
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-08-23 18:52:50 +0100

    adds basic styling

--- client/src/components/EquipmentItem.js
+++ client/src/components/EquipmentItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EquipmentItem = ({equip}) => (
     <div className="equipment-entry">
-      <li className="equipment">{equip.ExternalId}</li>
+      <li className="equipment" id="equipment-id">{equip.ExternalId}</li>
       <li className="equipment">{equip.TypeExternalId}</li>
       <li className="equipment">{equip.TypeDescription}</li>
     </div>
